7a9c172ca5b57461242c305fa2dfa7b69a60cd0a
mhjin <hahahia9224@gmail.com>
2019-04-20 23:35:55 +0900

    change log format

---
+++ src/main/java/com/example/bootApp/configuration/GeneralConfiguration.java
@@ -0,0 +1,24 @@
+package com.example.bootApp.configuration;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.beans.factory.config.PropertiesFactoryBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.ClassPathResource;
+
+@Slf4j
+@Configuration
+public class GeneralConfiguration {
+
+	@Value("${spring.profiles.active:local}")
+	private String profile;
+
+	@Bean
+	public PropertiesFactoryBean propertiesFactoryBean() {
+		PropertiesFactoryBean propertiesFactoryBean = new PropertiesFactoryBean();
+		propertiesFactoryBean.setLocation(new ClassPathResource(String.format("application-%s.yml", profile)));
+
+		return propertiesFactoryBean;
+	}
+}
