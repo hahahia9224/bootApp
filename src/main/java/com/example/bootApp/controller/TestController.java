fec893afe565045684968319dba059b5ab0a8ef1
iyaa.cs <iyaa.cs@gmail.com>
2019-04-21 20:57:58 +0900

    Mysql CRUD + JPA Intergration

--- src/main/java/com/example/bootApp/controller/TestController.java
+++ src/main/java/com/example/bootApp/controller/TestController.java
@@ -1,11 +1,11 @@
 package com.example.bootApp.controller;
 
 import com.example.bootApp.model.Test;
+import com.example.bootApp.service.MyService;
 import com.example.bootApp.service.TestService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -14,14 +14,28 @@ import java.util.List;
 public class TestController {
 
 	private TestService testService;
-
+	private MyService myService;
 	@Autowired
-	public TestController(TestService testService) {
+	public TestController(TestService testService, MyService myService) {
+
 		this.testService = testService;
+		this.myService=myService;
 	}
 
 	@GetMapping("/strings")
 	public List<Test> getStrings() {
 		return testService.getStrings();
 	}
+
+	@GetMapping("/insert")
+	public String AddValue(){
+		myService.InsertValues();
+		return "End";
+	}
+
+	@RequestMapping(value = "/Get", method= RequestMethod.GET)
+	public String AddValue(@RequestParam("val") String val){
+		return myService.GetByLast(val).toString();
+	}
+
 }
